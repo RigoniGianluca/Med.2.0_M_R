6aae25de1a831643dffcb06b223d4ec76382a225
Rigoni <8293360@itisrossi.vi.it>
2024-02-29 11:40:25 +0100

    .

---
+++ app/src/main/java/com/tpsit/med_20_r_m/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.tpsit.med_20_r_m
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val usernameText: TextInputEditText = findViewById(R.id.usernameInput)
+        val passwordText: TextInputEditText = findViewById(R.id.passwordInput)
+        val loginbutton: Button = findViewById(R.id.loginButton)
+
+        loginbutton.setOnClickListener {
+            val username = usernameText.text.toString()
+            val password = usernameText.text.toString()
+        }
+    }
+}
